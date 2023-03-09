de97e2b243d98a8476841b7030cb1daeb3501d68
matiurquiz <121512681+matiurquiz@users.noreply.github.com>
2023-03-09 11:07:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+alert ('bienvenides a Krusty Burger')
+alert ('El lugar de las mejores burgers, si desea hot dog ir a www.krustydog.com')
+
+let edadUsuario= parseInt (prompt ('Que edad tiene ud?'))
+
+let edadPermitida= 21
+
+if(edadUsuario >= edadPermitida) {
+    
+alert ('adelante ud.')
+
+
+
+}else{
+    alert('largate niño')
+    alert('patrañas ud puede pedir')
+
+}
+
+let combo= prompt('Para burger con queso ingrese 1 o para cangreburger ingrese 2')
+
+let combo1= 1
+
+
+if(combo<=1){
+    alert('muchas gracias x su compra su pedido ha sido tomado con exito')
+}else {
+    alert('largate niño, esto es krusty')
+}
+
+
+
